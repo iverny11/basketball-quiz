4f3f75a4b461b9d2378c20a373da9cd9da8fd780
iverny11 <78886231+iverny11@users.noreply.github.com>
2021-05-21 11:54:04 +1200

    Update instructions_v3.py

--- instructions_v3.py
+++ instructions_v3.py
@@ -1,16 +1,15 @@
 #instructions v3
 
 while True:
-   
-    inst = input("Would you like to read the instructions?\n a)Yes\n b)no\nEnter here: ")
-   
-    if inst == 'Yes' or inst == 'yes' or inst == 'A' or inst == 'a' or inst == 'Y' or inst == 'y' or inst == 'YES' :
-        print("The instructions are simple, you will be given random multiple choice questions about basketball and you have to choose which answer you think is right. ")
-        break
-    if inst == 'No' or inst == 'no' or inst == 'B' or inst == 'b' or inst == 'n' or inst == 'N' or inst =='NO':
-        print("Welcome to Isaac's basketball quiz, have fun!")
-        break
-    else:
-        print("**************************************")
-        print("Please enter the options provided only")
-        print("**************************************")
+                        inst = input("Would you like to read the instructions?\na)yes\nb)no\nEnter here: ")
+                        if inst == "yes" or inst == "y" or inst == "A" or inst == "a":
+                                    print("""The instructions are simple, you will be given random multiple choice general
+knowledge questions about basketball and you have to choose which answer you think is right.
+Answer the multiple choice questions by entering (a/b/c/d). For every question you get right you earn a point.
+""")
+                                    break
+                        if inst == "no" or inst == "n" or inst == "B" or inst == "b" :
+                                    print("Welcome to Isaac's basketball quiz")
+                                    break
+                        else:
+                                    print("Please enter the options")
