f1affaf108cfee78838fc4db1ef55896e5169636
iverny11 <78886231+iverny11@users.noreply.github.com>
2021-05-31 10:14:46 +1200

    Update status.py

--- status.py
+++ status.py
@@ -1,8 +1,9 @@
 #define the function
 def status():
-            status = input("Would you like to play the quiz?\n Enter y to continue or n to exit the quiz : ").lower()
+            status = input("Would you like to play the quiz?\nEnter y to continue or any other key to exit the quiz : ").lower()
             if status == "y" or status == "yes":
                         print("Thank you, we shall begin the General Knowledge Basketball Quiz shortly. \n")
             else:
                         print("Thank you for taking the time to use my program.")
                         exit()
+status()
