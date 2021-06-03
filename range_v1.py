8989c3fa334204d370edbfc6cb7e359dd8531699
iverny11 <78886231+iverny11@users.noreply.github.com>
2021-06-03 13:24:42 +1200

    Delete range_v1.py

--- range_v1.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        rounds = int(input("\nPlease enter how many rounds you want to paly maximum is 10 rounds : "))
-        if 0<rounds<=15:
-            break
-        else:
-            print("Please only enter the rounds 1-10")
-        except:
